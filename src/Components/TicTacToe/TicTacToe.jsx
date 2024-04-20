7d5a833551185ed7c63c40b4d0566164a43439f8
shjnd <kishandudhat2315@gmail.com>
2024-04-20 20:47:16 +0530

    UI completed

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -5,6 +5,26 @@ import cross_icon from '../../assets/cross.png'
 
 export const TicTacToe = () => {
   return (
-    <div>TicTacToe</div>
+    <div className='container'>
+      <h1 className='title'>Tic Tac Toe Game in <span>React</span></h1>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
+      <button className='reset'>Reset</button>
+    </div>
   )
 }
