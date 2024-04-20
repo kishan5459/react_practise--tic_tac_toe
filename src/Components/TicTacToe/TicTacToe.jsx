f7408dcce4de65a2d99a0c154c3478137822d822
shjnd <kishandudhat2315@gmail.com>
2024-04-20 14:01:00 +0530

    components are created and unnecessary files are removed

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TicTacToe.css'
+import circle_icon from '../../assets/circle.png'
+import cross_icon from '../../assets/cross.png'
+
+export const TicTacToe = () => {
+  return (
+    <div>TicTacToe</div>
+  )
+}
